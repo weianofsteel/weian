cc48001d3f62830247c9eaeaf0a32e82f15fe4b5
Weian Wang <weian.wang@wakeasp.com.tw>
2021-02-01 14:04:34 +0800

    add

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4fbfde4f2a48e7f33235";
+/******/ 	var hotCurrentHash = "bde0255cef7b4bc48778";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;

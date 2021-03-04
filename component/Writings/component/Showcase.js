67a5f591a172e596521fb812998e37584d4dc689
Weian Wang <weian.wang@wakeasp.com.tw>
2021-03-04 18:50:59 +0800

    showcase

--- component/Writings/component/Showcase.js
+++ component/Writings/component/Showcase.js
@@ -6,6 +6,9 @@ import { i18n, withTranslation } from '../../../i18n'
 import PropTypes from 'prop-types'
 
 const useStyles = makeStyles({
+    showcase: {
+        marginTop:'1rem'
+    }, 
     link: {
         backgroundColor: '#F5F5F5',
         height: '100%',
@@ -52,6 +55,53 @@ const Showcase = ({t}) => {
                 underline='none'
                 target='_blank' 
                 rel='noopener'
+                className={classes.showcase}
+            >
+                <div 
+                    className={box1===true?classes.link:classes.linkOnHover}
+                    onMouseEnter={handleBox1On}
+                    onMouseLeave={handleMouseLeave}
+                >
+                <Grid container>
+                    <Grid item xs={5}>
+
+                    </Grid>
+                    <Grid item xs={7}>
+                        <p className={box1===true?classes.title:classes.titleOnHover}>Exploring Next.js : multi-lingual support with next-i18next</p>
+                    </Grid>
+                </Grid>
+                </div>
+            </Link>
+
+            <Link 
+                href='./'
+                underline='none'
+                target='_blank' 
+                rel='noopener'
+                className={classes.showcase}
+            >
+                <div 
+                    className={box1===true?classes.link:classes.linkOnHover}
+                    onMouseEnter={handleBox1On}
+                    onMouseLeave={handleMouseLeave}
+                >
+                <Grid container>
+                    <Grid item xs={5}>
+
+                    </Grid>
+                    <Grid item xs={7}>
+                        <p className={box1===true?classes.title:classes.titleOnHover}>Exploring Next.js : multi-lingual support with next-i18next</p>
+                    </Grid>
+                </Grid>
+                </div>
+            </Link>
+
+            <Link 
+                href='./'
+                underline='none'
+                target='_blank' 
+                rel='noopener'
+                className={classes.showcase}
             >
                 <div 
                     className={box1===true?classes.link:classes.linkOnHover}

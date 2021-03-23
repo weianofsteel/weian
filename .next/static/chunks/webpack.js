c8cacff40696b5170106deafd2970d59718b8b3e
Weian Wang <weian.wang@wakeasp.com.tw>
2021-03-23 13:42:13 +0800

    add json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "29d010c9aa631e5da712";
+/******/ 	var hotCurrentHash = "252363edcb9be0f8dbd8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ const HomePage = ({ t }) =>{
 }
 
 HomePage.getInitialProps = async () => ({
-    namespacesRequired: ['header', 'banner', 'footer', 'skill', 'learnByDoing'],
+    namespacesRequired: ['header', 'banner', 'footer', 'skill', 'learnByDoing', 'sharing'],
   })
   
 HomePage.propTypes = {
